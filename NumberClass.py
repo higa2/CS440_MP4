7b196d3cf4b1d2d4743573858e27f7b2d3a64dde
ssaini4 <ssaini4@illinois.edu>
2015-12-05 18:48:19 -0600

    learning done

--- NumberClass.py
+++ NumberClass.py
@@ -8,16 +8,31 @@ class NumberClass:
         self.Feature = []
         self.weightVector = []
         self.count = 0
+        self.changed  = 0
         for i in range(784):
             self.Feature.append(0)
-            self.weightVector.append(1)
+            self.weightVector.append(0.5)
     
+    def addToFeature(self, adder, positionx, positiony):
+        self.Feature[positionx+28*positiony] += adder
     def updateWeightVector(self, adder, positiony, positionx):
-        temp = self.weightVector[positionx+positiony*28]
-        self.weightVector[positionx+positiony*28] = temp*adder+temp
+        temp = self.Feature[positionx+positiony*28]
+        self.weightVector[positionx+positiony*28] += temp*adder 
     
     def addToCount(self, adder):
         self.count += adder
         
+    def getWeights(self):
+        return self.weightVector
+    
+    def getFeature(self):
+        return self.Feature
+    
     def getWeightVector(self, positiony, positionx):
         return self.weightVector[positionx+positiony*28]
+    
+    def getStatus(self):
+        return self.changed
+    
+    def changeStatus(self):
+        self.changed = 1
